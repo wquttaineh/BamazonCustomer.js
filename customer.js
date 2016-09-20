9eb8ece1dd5015e89f3459416c9ee1ba72b17440
Fiona Ballard <fionaballard@Fiona-2.local>
2016-09-20 19:32:16 -0400

    updated customer.js

--- customer.js
+++ customer.js
@@ -1,6 +1,10 @@
+// npm inquirer package 
 var inquirer = require('inquirer');
+
+// npm mysql package 
 var mysql = require('mysql');
 
+// connection host to mysql 
 var connection = mysql.createConnection({
 	host: 		'127.0.0.1',
 	port:        8889, 
@@ -8,12 +12,15 @@ var connection = mysql.createConnection({
 	password: 	'root',
 	database: 	'Bamazon'
 });
+
 var cost =0;
 var total = 0;
-// var tax = 0;
 var totalcount = 0;
 
+// function to start application.
 function pickId(){
+
+	// mysql 
 	connection.query('SELECT * FROM products', function(err, rows, fields){
 		if (err) throw err;
 		console.log('What would you like to buy?');
@@ -24,7 +31,7 @@ function pickId(){
 				inquirer.prompt([{
 					type: 'input',
 					name: 'itemById',
-					message: 'Select ID of the item you would like to buy!'
+					message: 'Select ID # of the item you would like to buy!'
 
 				}]).then(function(answer){
 					var itemId = parseInt(answer.itemById)
@@ -33,12 +40,12 @@ function pickId(){
 					for (var i = 0; i < rows.length; i++) {
 						if(rows[i].itemID == answer.itemById){
 							var row = rows[i]; 
-							console.log('We have ' + row.StockQuantity + ' items in stock, and its only $' + row.Price + ' per Item!');
+							console.log('We have ' + row.StockQuantity + ' ' +row.ProdcutName + ' in stock, and its only $' + row.Price + ' per Item!');
 
 							inquirer.prompt([{
 								type: 'input',
 								name: 'itemQuantity',
-								message: 'How many would you like to buy?'
+								message: 'How many ' + row.ProdcutName + ' would you like to buy?'
 
 							}]).then(function(answer){
 								var quantity = parseInt(answer.itemQuantity);
@@ -71,7 +78,7 @@ function pickId(){
 									var tax = ((.65 / 10000) * 1000) * totalcount;
 									var total = totalcount + tax;
 									
-									console.log('Total Count: ' + quantity + ' items * ' + "'$" + cost + "'");
+									console.log('Total Count: ' + quantity + ' ' +row.ProdcutName + '  * ' + "'$" + cost + "'");
 									console.log('Pice:  $' + totalcount);
 									console.log('Tax @ 0.065%: $' + tax);
 									console.log('your total balance is:  $' + total);
